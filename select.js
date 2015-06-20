12c68f528a8312fff99ea419584f0e7c94c6059d
Ryan Frederick <ryan@ryanfrederick.com>
2015-06-20 14:17:27 -0700

    Polish and add comments.

--- select.js
+++ select.js
@@ -1,23 +1,38 @@
 define(['$'], function($) {
+    //
+    // Component constructor
+    //
     var Select = function Select($el, options) {
-        this.dom = {
-            $root: $el,
-            $value: $el.find('.c-select__value')
-        };
+        // Component instance properties. The `$` hash (by convention) caches
+        // the root node and any sub-components.
         this.value = '';
+        this.$ = {
+            root: $el,
+            value: $el.find('.c-select__value')
+        };
 
-        this.dom.$root.on('change', $.proxy(this.update, this));
+        // Event handlers should use $.proxy to preserve access to the instance
+        // as `this` within the handler.
+        this.$.root.on('change', $.proxy(this.update, this));
 
+        // Immediately invoke any methods needed to set up initial state.
         this.update();
     }
 
+    //
+    // Update the component’s visible value when the underlying select element’s
+    // value changes.
+    //
     Select.prototype.update = function update() {
-        this.dom.$value.text(this.dom.$root.find('option:selected').text());
-        this.dom.$root.trigger('select:update'); // notify subscribers
+        this.value = this.$.root.find('option:selected').text();
+        this.$.value.text(this.value);
+        this.$.root.trigger('select:update'); // notify subscribers
     }
 
     return {
         'init': function($el, options) {
+            // If already initialized, return the instance; otherwise, create it
+            // and expose it through `$('.c-select').data('component')`.
             return $el.data('component') || $el.data('component', new Select($el, options));
         }
     };

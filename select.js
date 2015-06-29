8fb2306dbb78b5939a5154e7bc7bd50e90256185
Ryan Frederick <ryan@ryanfrederick.com>
2015-06-29 11:42:41 -0700

    Remove weird conventions and fix event emitting.
    - Remove `this.dom.$value` in favour of `this.$valueElement`
    - Ensure the `this` within `trigger` for emitted events is bound to the instance
    - Pass data with emitted events

--- select.js
+++ select.js
@@ -1,34 +1,38 @@
 define(['$'], function($) {
-    //
-    // Component constructor
-    //
+    /**
+     * Constructor
+     */
     var Select = function Select($el, options) {
-        // Component instance properties. The `$` hash (by convention) caches
-        // the root node and any sub-components.
+        // Component instance properties.
         this.value = '';
-        this.dom = {
-            $root: $el,
-            $value: $el.find('.c-select__value')
-        };
+        this.$el: $el;
+        this.$valueElement: $el.find('.c-select__value');
 
         // Event handlers should use $.proxy to preserve access to the instance
         // as `this` within the handler.
-        this.dom.$root.on('change', $.proxy(this.update, this));
+        this.$el.on('change', $.proxy(this.update, this));
 
         // Immediately invoke any methods needed to set up initial state.
         this.update();
     }
 
-    //
-    // Update the component’s visible value when the underlying select element’s
-    // value changes.
-    //
+    /**
+     * Update the component’s visible value when the underlying select element’s
+     * value changes.
+     */
     Select.prototype.update = function update() {
-        this.value = this.dom.$root.find('option:selected').text();
-        this.dom.$value.text(this.value);
-        this.dom.$root.trigger('select:update'); // notify subscribers
+        this.value = this.$el.find('option:selected').text();
+        this.$valueElement.text(this.value);
+
+        // Emit events
+        $.proxy(this.$el.trigger('select:update', {
+            value: this.value,
+        }), this);
     }
 
+    /**
+     * Export the init method required by AdaptiveJS.
+     */
     return {
         'init': function($el, options) {
             // If already initialized, return the instance; otherwise, create it

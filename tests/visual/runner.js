cd44193550c76d197e4002650521567c42ead93e
Ryan Frederick <ryan@ryanfrederick.com>
2015-07-21 14:06:54 -0700

    Move RequireJS config into runner.js
    - This brings it in line with the code structure created by the
      Stencil generator.

--- tests/visual/runner.js
+++ tests/visual/runner.js
@@ -1,3 +1,19 @@
+require.config({
+    paths: {
+        'dust-full': '../../node_modules/dustjs-linkedin/dist/dust-full',
+        'adaptivejs': '../../node_modules/adaptivejs',
+        '$': '../../node_modules/jquery/dist/jquery',
+    },
+    shim: {
+        'dust-full': {
+            'exports': 'dust'
+        },
+        '$': {
+            'exports': 'jQuery'
+        }
+    },
+});
+
 define(function(require) {
     var dust = require('dust-full');
     var componentHelper = require('adaptivejs/lib/dust-component-helper');
